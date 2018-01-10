17cf92394912784abfb5381449780bf2e66dea7e
spencer <spencajones@gmail.com>
2018-01-10 15:16:04 -0700

    first push webpack setup with lint and babel and production/development configurations

---
+++ app/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App () {
+  return (
+    <div>
+
+      {'hello world'}
+
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
---
+++ webpack.config.babel.js
@@ -0,0 +1,66 @@
+import path from 'path'
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const LAUNCHCOMMAND = process.env.npm_lifecycle_event
+
+const isProduction = LAUNCHCOMMAND === 'production'
+process.env.BABEL_ENV = LAUNCHCOMMAND
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'dist'),
+}
+
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: PATHS.app + '/index.html',
+  filename: 'index.html',
+  inject: 'body',
+})
+
+const productionPlugin = new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production'),
+  },
+})
+
+const base = {
+  entry: [
+    PATHS.app,
+  ],
+  output: {
+    path: PATHS.build,
+    filename: 'index_bundle.js',
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.css$/, loader: 'style-loader!css-loader?sourceMap&modules&localIdentName=[name]__[local]___[hash:base64:5]'},
+    ],
+  },
+  resolve: {
+    modules: [
+      path.resolve(__dirname, './app'),
+      'node_modules',
+    ],
+  },
+}
+
+const developmentConfig = {
+  devtool: 'cheap-module-inline-source-map',
+  devServer: {
+    contentBase: PATHS.build,
+    hot: true,
+    inline: true,
+    progress: true,
+    historyApiFallback: true,
+  },
+  plugins: [HTMLWebpackPluginConfig, new webpack.HotModuleReplacementPlugin()],
+}
+
+const productionConfig = {
+  devtool: 'cheap-module-source-map',
+  plugins: [HTMLWebpackPluginConfig, productionPlugin],
+}
+
+module.exports = Object.assign({}, base, isProduction ? productionConfig : developmentConfig)
